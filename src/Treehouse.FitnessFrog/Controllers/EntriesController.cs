90f257dcbeb05c7be50d56d84cbfe1116b124449
Alejandro lopera sanchez <alelopera@hotmail.com>
2019-04-09 12:42:50 -0500

    Added validation rules using ModelState

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -55,6 +55,11 @@ namespace Treehouse.FitnessFrog.Controllers
         [HttpPost]
         public ActionResult Add(Entry entry)
         {
+            if (ModelState.IsValidField("Duration") && entry.Duration >=0)
+            {
+                ModelState.AddModelError("Duration", "El campo duración debe ser mayor que cero.");
+            }
+
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
